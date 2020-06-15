587637b8eee996cbac3a5e06d5962ae38d070128
sabyasachi07 <sahoosabyasachi07@gmail.com>
2020-06-15 23:57:58 +0530

    ssl-layer

--- src/core/serverpart.cpp
+++ src/core/serverpart.cpp
@@ -1,36 +1,81 @@
 #include <QLoggingCategory>
-
+#include <QSslSocket>
+#include <QFile>
 #include "serverpart.h"
 
 Q_LOGGING_CATEGORY(ATCORE_SERVER, "org.kde.atelier.core.server")
 
-ServerPart::ServerPart(QObject *parent):QObject(parent)
+ServerPart::ServerPart(QObject *parent):QTcpServer(parent),m_nNextBlockSize(0)
 {
-      m_tcpServer=new QTcpServer(this);
+
+        QFile keyFile(QLatin1String("../../certificates/red_local.key"));
+        keyFile.open(QIODevice::ReadOnly);
+        key = QSslKey(keyFile.readAll(), QSsl::Rsa , QSsl::Pem);
+        keyFile.close();
+
+        QFile certFile(QLatin1String("../../certificates/red_local.pem"));
+        certFile.open(QIODevice::ReadOnly);
+        cert = QSslCertificate(certFile.readAll());
+        certFile.close();
+
+        if (!listen(QHostAddress(tr("127.0.0.1")), 12345)) { // FQDN in red_local.pem is set to 127.0.0.1.  If you change this, it will not authenticate.
+            qCritical() << "Unable to start the TCP server";
+            exit(0);
+        }
+        connect(this, &ServerPart::newConnection , this , &ServerPart::newConnection);
+
 }
 
-void ServerPart::startserver()
+
+void ServerPart::incomingConnection(qintptr sslSocketDescriptor)
 {
-     if (!m_tcpServer->listen(QHostAddress::Any, 37369)){
-      qCDebug(ATCORE_SERVER) <<   (tr("Error! The port is taken by some other service."));
+    QSslSocket *sslSocket = new QSslSocket(this);
+
+    connect(sslSocket,QOverload<const QList<QSslError> &>::of(&QSslSocket::sslErrors),this,&ServerPart::sslErrors);
+    sslSocket->setSocketDescriptor(sslSocketDescriptor);
+    sslSocket->setLocalCertificate(cert.toText(),QSsl::Pem);
+    sslSocket->setPrivateKey(key);
+    sslSocket->addCaCertificates(QLatin1String("../../certificates/blue_ca.pem"));
+    sslSocket->setPeerVerifyMode(QSslSocket::VerifyNone);
+    sslSocket->setProtocol(QSsl::TlsV1SslV3);
+
+    addPendingConnection(sslSocket);
+}
+
+
+
+
+/*void ServerPart::startserver()
+{
+      if (!m_tcpServer->listen(QHostAddress::Any, 38373)){
+      qCDebug(ATCORE_SERVER) <<(tr("Error! The port is taken by some other service."));
       return;
       }
       connect(m_tcpServer, &QTcpServer::newConnection, this, &ServerPart::newConnection);
       qCDebug(ATCORE_SERVER) << (tr("Server started, port is openned."));
-}
+}*/
 
+void ServerPart::sslErrors(const QList<QSslError> &errors)
+{
+
+    foreach (const QSslError &error, errors)
+     qCDebug(ATCORE_SERVER) << error.errorString();
+}
 void ServerPart::newConnection()
 {
-        QTcpSocket *m_clientSocket = m_tcpServer->nextPendingConnection();
+
+
+       QTcpSocket *m_clientSocket = nextPendingConnection();
 
        connect(m_clientSocket, &QTcpSocket::readyRead, this, [this,m_clientSocket](){readClient(m_clientSocket);});
        connect(m_clientSocket, &QTcpSocket::disconnected, this, [this,m_clientSocket](){disconnect(m_clientSocket);});
        qCDebug(ATCORE_SERVER) << tr("connection established");
+
 }
 
-void ServerPart::readClient(QTcpSocket *clientSocket)
+void ServerPart::readClient(QTcpSocket *m_clientSocket)
 {
-    //here socket willl read from the client
+    //here ssslSocket willl read from the client
       QDataStream in(m_clientSocket);
        for (;;){
            if(!m_nNextBlockSize){
@@ -40,12 +85,12 @@ void ServerPart::readClient(QTcpSocket *clientSocket)
             if (m_clientSocket->bytesAvailable() < m_nNextBlockSize) { break; }
             QString str;
             in >> str;
-            emit gotNewCommand(str);
             m_nNextBlockSize = 0;
+            qDebug()<< str;
        }
 }
 
-void ServerPart::disconnect(QTcpSocket* clientSocket)
+void ServerPart::disconnect(QTcpSocket* m_clientSocket)
 {
        //here it will be disconnected
        qCDebug(ATCORE_SERVER) <<tr("disconnect");
--- src/core/serverpart.h
+++ src/core/serverpart.h
@@ -1,29 +1,42 @@
 #pragma once
-
+#include <QSslSocket>
 #include <QTcpServer>
 #include <QTcpSocket>
 #include <QDataStream>
 #include <QObject>
+#include <QSslKey>
+#include <QSslCertificate>
+#include <QFile>
+#include <QSslSocket>
+#include "atcore_export.h"
 
-class  ServerPart :public QObject
+
+class QSslSocket;
+class QFile;
+
+class  ATCORE_EXPORT ServerPart :public QTcpServer
 {
     Q_OBJECT
 
 public:
   ServerPart(QObject *parent = nullptr);
-  QTcpServer *m_tcpServer;
 
 
 signals :
     void gotNewCommand(QString str);
 
 public slots:
-    void startserver();
-    void newConnection();
+    void sslErrors(const QList<QSslError> &errors);
+     //void startserver();
+     virtual void newConnection();
     void readClient(QTcpSocket* m_clientSocket);
     void disconnect(QTcpSocket* m_clientSocket);
 
 private:
     quint16 m_nNextBlockSize;
-};
+    QSslKey key;
+    QSslCertificate cert;
 
+protected:
+    void incomingConnection(qintptr sslSocketDescriptor);
+};
